88d66da5d2b7f54f9759bda08ded07c1549bbf7e
JayR <goldenjayr@gmail.com>
2019-11-26 09:01:28 +0800

    added graphql endpoints

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphqlHttp = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const app = express()
 
 app.use(bodyParser.json())
+// query = fetching data
+// mutation = changing data
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
 
-app.get('/', (req, res, next)=>{
-    res.send('Hello World!')
-})
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Reading', 'Washing', 'Cooking']
+        },
+        createEvent: (args) => {
+            const eventName = args.name
+            return eventName
+        }
+    },
+    graphiql: true
+}))
 
 app.listen(3000)
